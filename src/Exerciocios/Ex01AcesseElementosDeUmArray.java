c295e84d7f3a19f43ba355ff1b2454a0ebe0003f
Joao P Manfredini <jpaulohm@gmail.com>
2022-09-22 20:20:51 -0300

    Ex01 Finalizado

--- src/Exerciocios/Ex01AcesseElementosDeUmArray.java
+++ src/Exerciocios/Ex01AcesseElementosDeUmArray.java
@@ -1,4 +1,12 @@
 package Exerciocios;
 
 public class Ex01AcesseElementosDeUmArray {
+    public int ImprimirElemento(int posicao, int[] conjunto){
+        System.out.println(conjunto[posicao]);
+        return conjunto[posicao];
+    }
+    public int ImprimirUltimoElemento(int[] conjunto){
+        System.out.println(conjunto[conjunto.length-1]);
+        return conjunto[conjunto.length-1];
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import Exerciocios.Ex01AcesseElementosDeUmArray;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = { 56, 57, 58, 67, 99, 12, 11, 88 };
+
+        Ex01AcesseElementosDeUmArray ex01 =  new Ex01AcesseElementosDeUmArray();
+
+        System.out.printf("1)\na)");
+        ex01.ImprimirElemento(0,arr);
+
+        System.out.printf("b)");
+        ex01.ImprimirUltimoElemento(arr);
+
+        System.out.printf("c)");
+        ex01.ImprimirElemento(2,arr);
+
+
     }
 }
